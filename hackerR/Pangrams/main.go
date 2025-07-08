6d531a4332e10ea8c824e960dcdb880c3823793f
Ing. Cesar Fernandez <service@cesarlead.com>
2025-07-08 10:46:36 -0400

    Feat: ejercicio Pangrams

--- hackerR/Pangrams/main.go
+++ hackerR/Pangrams/main.go
@@ -1,7 +1,31 @@
 package main
 
+import "fmt"
+
 // Pangrams
+func isPangram(phrase string) string {
+	alphabet := [26]bool{}
+	for _, char := range phrase {
+		if char >= 'a' && char <= 'z' {
+			alphabet[char-'a'] = true
+		}
+	}
+
+	for _, value := range alphabet {
+		if !value {
+			return "not pangram"
+		}
+	}
+
+	return "pangram"
+}
 
 func main() {
+
+	phrase := "the quick brown fox jumps over the lazy dog"
+	fmt.Println(isPangram(phrase))
+
+	phrase = "the quick brown fox jumps over the lazy cat"
+	fmt.Println(isPangram(phrase))
 
 }
